47212c644d62a859cacdda8786a968c0a0de9341
ziele3920 <a.ziolkowski3920@wp.pl>
2019-08-11 12:31:36 +0200

    Pair

---
+++ src/main/java/Pair/MainPair.java
@@ -0,0 +1,9 @@
+package Pair;
+
+public class MainPair {
+    public static void main(String[] args) {
+        //Pair<Integer, String> pair = new Pair(2, "dwa");
+        Pair<String, String> pair2 = new Pair<>("1", "1");
+        String string2 = pair2.getKey();
+    }
+}
